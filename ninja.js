b166b7e106c187b33823c345c7921177f1a8ca20
Matthew J. Moldovan <matthewmoldovan@outlook.com>
2023-03-07 13:00:05 -0700

    Added Sensei class

--- ninja.js
+++ ninja.js
@@ -21,9 +21,30 @@ class Ninja {
     }
 }
 
+
+class Sensei extends Ninja{
+    constructor(name){
+        super(name)
+        this.health = 200
+        this.speed = 10
+        this.strength = 10
+        this.wisdom = 10
+    }
+
+    speakWisdom(){
+        this.drinkSake();
+        console.log('What one programmer can do in one month, two programmers can do in two months.')
+    }
+}
+
 const ninja1 = new Ninja("Matei",90)
 
 ninja1.sayName();
 ninja1.showStats();
 ninja1.drinkSake();
 ninja1.showStats();
+
+const sensei1 = new Sensei("Neil")
+
+sensei1.speakWisdom();
+sensei1.showStats();
