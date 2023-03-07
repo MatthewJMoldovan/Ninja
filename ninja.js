225799dcd9a6cb821e23ec92b1962465d48c96b8
Matthew J. Moldovan <matthewmoldovan@outlook.com>
2023-03-07 12:53:29 -0700

    ninja_js_oop

---
+++ ninja.js
@@ -0,0 +1,29 @@
+class Ninja {
+    constructor(name,health){
+        this.name = name;
+        this.health = health;
+        this.speed = 3;
+        this.strength = 3;
+    }
+
+    sayName(){
+        console.log(this.name);
+    }
+
+    showStats(){
+        console.log(
+            `Name: ${this.name} - Health: ${this.health} - Strength: ${this.strength} - Speed: ${this.speed}`
+        )
+    }
+
+    drinkSake(){
+        this.health += 10;
+    }
+}
+
+const ninja1 = new Ninja("Matei",90)
+
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
+ninja1.showStats();
